f52137237e12a28caf9b156aed4941cd2d0be6bb
Marcos-VNC <marcosvn.cardoso@outlook.com>
2021-07-05 10:42:12 -0300

    Segundo commit

--- home/urls.py
+++ home/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='home'),
     path('cadastrar/', views.cad_user, name='user'),
     path('login/', views.login_user, name='login'),
     path('reserva/', views.reserva, name='reserva'),
